8f3f676c9d8adeb28c4dbdfce48a5b052cf25d24
Kyle Vincent <kyle.vincent@my.jcu.edu.au>
2018-09-10 13:48:34 +1000

    got the data pulling system started
    -can now view data in a div/rows
    - it looks really nice!

--- public/root/vendor/js/script.js
+++ public/root/vendor/js/script.js
@@ -1,4 +1,20 @@
 
+//<editor-fold desc="LOADING: Firebase init code">
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyApi3weoa0PPvli41nrx_p7Jir4BzGwFm0",
+    authDomain: "careerhubv2.firebaseapp.com",
+    databaseURL: "https://careerhubv2.firebaseio.com",
+    projectId: "careerhubv2",
+    storageBucket: "careerhubv2.appspot.com",
+    messagingSenderId: "67408577281"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+var ref = database.ref('jobs');
+//</editor-fold>
+
 //<editor-fold desc="FUNCTION: For loading/fading the screen loading gif">
 jQuery(window).on('load',function() {
     // Animate loader off screen
@@ -62,151 +78,6 @@ function goToStudentDash(){
 }
 //</editor-fold>
 
-// function CreateTableFromJSON() {
-//
-//
-//     var myBooks = $.getJSON('../vendor/res/db/db.json');
-//
-//     // EXTRACT VALUE FOR HTML HEADER.
-//     // ('Book ID', 'Book Name', 'Category' and 'Price')
-//     var col = [];
-//     for (var i = 0; i < myBooks.length; i++) {
-//         for (var key in myBooks[i]) {
-//             if (col.indexOf(key) === -1) {
-//                 col.push(key);
-//             }
-//         }
-//     }
-//
-//     // CREATE DYNAMIC TABLE.
-//     var table = document.createElement("table");
-//
-//     // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-//
-//     var tr = table.insertRow(-1);                   // TABLE ROW.
-//
-//     for (var i = 0; i < col.length; i++) {
-//         var th = document.createElement("th");      // TABLE HEADER.
-//         th.innerHTML = col[i];
-//         tr.appendChild(th);
-//     }
-//
-//     // ADD JSON DATA TO THE TABLE AS ROWS.
-//     for (var i = 0; i < myBooks.length; i++) {
-//
-//         tr = table.insertRow(-1);
-//
-//         for (var j = 0; j < col.length; j++) {
-//             var tabCell = tr.insertCell(-1);
-//             tabCell.innerHTML = myBooks[i][col[j]];
-//         }
-//     }
-//
-//     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-//     var divContainer = document.getElementById("showData");
-//     divContainer.innerHTML = "";
-//     divContainer.appendChild(table);
-// }
-
-//<editor-fold desc="MATTS CODE FOR DB">
-// // Add data from Firebase to table
-// firebase.database().ref('quotes').on('child_added', function (quoteSnapshot) {
-//     retrieveQuotes(quoteSnapshot)
-// });
-//
-// // Handles quotes that have been removed
-// firebase.database().ref('quotes').on('child_removed', function (quoteSnapshot) {
-//     removeQuote(quoteSnapshot)
-// });
-//
-//
-// function retrieveQuotes(quoteSnapshot) {
-//     var table = document.getElementById("quote-table-body");
-//
-//     var tr = document.createElement("tr");
-//
-//     var th = document.createElement("th");
-//     var scopeAtt = document.createAttribute("scope");
-//     scopeAtt.value = "row";
-//
-//     var idAtt = document.createAttribute("id");
-//     idAtt.value = quoteSnapshot.key;
-//
-//     var txt = document.createTextNode(quoteNum.toString());
-//     th.setAttributeNode(scopeAtt);
-//     tr.setAttributeNode(idAtt);
-//     th.appendChild(txt);
-//     tr.appendChild(th);
-//
-//     var td = document.createElement("td");
-//     txt = document.createTextNode(quoteSnapshot.val().author);
-//     td.appendChild(txt);
-//     tr.appendChild(td);
-//
-//     td = document.createElement("td");
-//     txt = document.createTextNode(quoteSnapshot.val().date);
-//     td.appendChild(txt);
-//     tr.appendChild(td);
-//
-//     td = document.createElement("td");
-//     txt = document.createTextNode(quoteSnapshot.val().quote);
-//     var em = document.createElement("em");
-//     em.appendChild(txt);
-//     td.appendChild(em);
-//     tr.appendChild(td);
-//
-//     table.appendChild(tr);
-//
-//     quoteNum += 1;
-// }
-//
-// function removeQuote(quoteSnapshot) {
-//     var parent = document.getElementById("quote-table-body");
-//     var child = document.getElementById(quoteSnapshot.key);
-//     parent.removeChild(child);
-// }
-//
-// function modifyQuote(quoteSnapshot) {
-//
-//     var tr = document.getElementById(quoteSnapshot.key);
-//
-//     var td = document.createElement("td");
-//     txt = document.createTextNode(quoteSnapshot.val().author);
-//     td.appendChild(txt);
-//     tr.appendChild(td);
-//
-//     td = document.createElement("td");
-//     txt = document.createTextNode(quoteSnapshot.val().date);
-//     td.appendChild(txt);
-//     tr.appendChild(td);
-//
-//     td = document.createElement("td");
-//     txt = document.createTextNode(quoteSnapshot.val().quote);
-//     var em = document.createElement("em");
-//     em.appendChild(txt);
-//     td.appendChild(em);
-//     tr.appendChild(td);
-//
-//     table.appendChild(tr);
-// }
-//
-//</editor-fold>
-
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyApi3weoa0PPvli41nrx_p7Jir4BzGwFm0",
-    authDomain: "careerhubv2.firebaseapp.com",
-    databaseURL: "https://careerhubv2.firebaseio.com",
-    projectId: "careerhubv2",
-    storageBucket: "careerhubv2.appspot.com",
-    messagingSenderId: "67408577281"
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-var ref = database.ref('jobs');
-
-
 
 function testButton(){
 
@@ -221,29 +92,70 @@ function testButton(){
 
     //Creating data
     var data= {
-        name: "goodname",
-        score: 43
+        title: "beer bois",
+        description: "We find the beer, we look at it for 3 seconds, then we drink"
     }
 
     //Pushes data to the DB
     ref.push(data);
 
+
+
+}
+
+
+//This if statement will check to see if the page is the student dashboard; If we are on the student dashboard
+// then run the job fetching code.
+if (document.title == "Student Dashboard"){
+    console.log("We Are On The Student Dashboard");
+}
+else{
+    console.log("We Are Not On The Student Dashboard");
+}
+
+function fetchJobs(){
     //This is getting the data based on weather or not it's a value
     ref.on('value', gotData, errData);
 }
 
 //logging the data that's got from the firebase ref.on call into the console
 function gotData(data){
-
-    //console.log(data.val());
     var jobs = data.val();
     var keys = Object.keys(jobs);
     console.log(keys);
+
+    //running through a for loop based on the ammount of items in the db
     for (var i = 0; i < keys.length; i++){
         var k = keys[i];
-        var initials = jobs[k].name;
-        var score = jobs[k].score;
-        console.log(initials, score);
+        var titl = jobs[k].title;
+        var desc = jobs[k].description;
+        console.log(titl, desc);
+
+        //create an element on the page for each job in the db
+        var main_container = document.createElement("div");
+        var left_container = document.createElement("div");
+        var right_container = document.createElement("div");
+        var leftText = document.createTextNode(titl);
+        var rightText = document.createTextNode(desc);
+
+        //adding ID's to each element
+        left_container.setAttribute("id", "jobDisplayDiv-post-title");
+        right_container.setAttribute("id", "jobDisplayDiv-post-desc");
+        main_container.setAttribute("id", "jobDisplayDiv-post-container");
+
+        //adding text to left/right divs
+        left_container.appendChild(leftText);
+        right_container.appendChild(rightText);
+
+        //adding containers to main div
+        main_container.appendChild(left_container);
+        main_container.appendChild(right_container);
+
+        //fetching the main div on the doc and adding the maincontainer to it
+        var mainJobDiv = document.getElementById("jobDisplayDiv");
+        mainJobDiv.appendChild(main_container);
+
+
     }
 }
 
